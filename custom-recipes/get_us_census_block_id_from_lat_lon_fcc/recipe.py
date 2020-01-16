cbc2aabb95e7d116ae804c012e6bfb74da3a1551
subodhgupta <subodhgupta@users.noreply.github.com>
2020-01-16 15:09:14 -0500

    Add files via upload

--- custom-recipes/get_us_census_block_id_from_lat_lon_fcc/recipe.py
+++ custom-recipes/get_us_census_block_id_from_lat_lon_fcc/recipe.py
@@ -2,31 +2,48 @@
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+
 import requests 
+import urllib
 import time
 from dataiku.customrecipe import *
 import sys
 
-print ('## Running Plugin v0.4.0 ##')
+#disable InsecureRequestWarning.
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+print ('## Running Plugin v0.5.0 ##')
 
 input_name = get_input_names_for_role('input')[0]
-
 
 # Recipe out
 
 output_ = get_output_names_for_role('output')[0]
 output_dataset = dataiku.Dataset(output_)
 
-schema = [{'name':'block_group','type':'string'}
-        ,{'name':'block_id','type':'string'}
-        ,{'name':'tract_id','type':'string'}
-        ,{'name':'county_id','type':'string'}
-        ,{'name':'county_name','type':'string'}
-        ,{'name':'lat','type':'string'}
-        ,{'name':'lon','type':'string'}
-        ,{'name':'state_code','type':'string'}
-        ,{'name':'state_id','type':'string'}
-        ,{'name':'state_name','type':'string'}]
+schema = [
+            {'name':'block_group','type':'string'}
+            ,{'name':'block_id','type':'string'}
+            ,{'name':'tract_id','type':'string'}
+            ,{'name':'county_id','type':'string'}
+            ,{'name':'county_name','type':'string'}
+            ,{'name':'lat','type':'string'}
+            ,{'name':'lon','type':'string'}
+            ,{'name':'state_code','type':'string'}
+            ,{'name':'state_id','type':'string'}
+            ,{'name':'state_name','type':'string'}
+            ,{'name':'state_centlat','type':'string'}    
+            ,{'name':'state_centlon','type':'string'}
+            ,{'name':'state_division','type':'string'}
+            ,{'name':'state_region','type':'string'}
+            ,{'name':'state_areawater','type':'string'}
+            ,{'name':'state_arealand','type':'string'}   
+            ,{'name':'county_basename','type':'string'}
+            ,{'name':'county_centlat','type':'string'}    
+            ,{'name':'county_centlon','type':'string'}
+            ,{'name':'county_areawater','type':'string'}
+            ,{'name':'county_arealand','type':'string'}     
+        ]
 
 
 P_PAUSE = int(get_recipe_config()['param_api_throttle'])
@@ -59,9 +76,7 @@ if get_recipe_config().get('p_id_column', None) is not None and get_recipe_confi
 else:
     use_column_id=False
 
-
 output_dataset.write_schema(schema)
-
 
 b=-1 
 with output_dataset.get_writer() as writer:
@@ -70,14 +85,12 @@ with output_dataset.get_writer() as writer:
         b = b +1
         n_b = b * P_BATCH_SIZE_UNIT 
 
-
         df = df[abs(df[P_LAT]>0) | abs(df[P_LON]>0)]
 
         if strategy =='make_unique':
             dfu = df.groupby([P_LAT,P_LON]).count().reset_index()
         else:
             dfu = df.copy()
-
 
         n__ = -1
         for v in dfu.to_dict('records'):
@@ -91,57 +104,97 @@ with output_dataset.get_writer() as writer:
             if use_column_id:
                 id_ = v[id_column]
 
-            
             print '%s - processing: (%s,%s)' % (n_record,lat, lon)
             
+            # p = {'format': 'json',
+            #      'y': lat,
+            #      'x': lon,
+            #      'benchmark': P_BENCHMARK,
+            #      'vintage': P_VINTAGE,
+            #      'layers': '10'
+            #      }
             
-            p={'format': 'json',
-                'y': lat,
-                'x': lon,
-                'benchmark': P_BENCHMARK, 
-                'vintage': P_VINTAGE,
-                'layers':'10'}
-            
+            #Encode parameters 
+            params = urllib.urlencode(
+                                        {'format': 'json',
+                                         'y': lat,
+                                         'x': lon,
+                                         'benchmark': P_BENCHMARK,
+                                         'vintage': P_VINTAGE
+                                         }
+                                     )
+            #Contruct request URL
+            url = 'https://geo.fcc.gov/api/census/block/find?' + params
             
             call = requests.get('https://geocoding.geo.census.gov/geocoder/geographies/coordinates', params=p,verify=False)
-
+            
             if call.status_code == 200:
 
                 data = call.json()
 
-
                 try:
-                    s_geo = data['result'][u'geographies'][u'Census Block Groups'][0]
+                    s_geo = data['result'][u'geographies'][u'2010 Census Blocks'][0]
 
                     d={}
-
-                    
+         
                     d['block_group'] = s_geo[u'GEOID']
-                    d['block_id'] = None
-                    d['tract_id'] = s_geo[u'GEOID'][:11]
-                    d['county_id'] = s_geo[u'COUNTY']
-                    d['county_name'] = None #Not available directly with this API: here for consistency.
+                    d['block_id'] = s_geo[u'BLOCK']
+                    d['tract_id'] = None
                     d['lat'] = lat
                     d['lon'] = lon
-                    d['state_code'] = None #Not available directly with this API: here for consistency.
-                    d['state_id'] = s_geo[u'STATE']
-                    d['state_name'] = None #Not available directly with this API: here for consistency.
+                    
+                    s_geo = data['result'][u'geographies'][u'States'][0]
+                    
+                    d['state_code'] = s_geo[u'STUSAB']
+                    d['state_id'] = s_geo[u'GEOID']
+                    d['state_name'] = s_geo[u'NAME']
+                    d['state_centlat'] = s_geo[u'CENTLAT']
+                    d['state_centlon'] = s_geo[u'CENTLON']
+                    d['state_division'] = s_geo[u'DIVISION']
+                    d['state_region'] = s_geo[u'REGION']
+                    d['state_areawater'] = s_geo[u'AREAWATER']
+                    d['state_arealand'] = s_geo[u'AREALAND']
+                    
+                    s_geo = data['result'][u'geographies'][u'Counties'][0]
+                    
+                    d['county_id'] = s_geo[u'GEOID']   
+                    d['county_name'] = s_geo[u'NAME']
+                    d['county_basename'] = s_geo[u'BASENAME']
+                    d['county_centlat'] = s_geo[u'CENTLAT']
+                    d['county_centlon'] = s_geo[u'CENTLON']
+                    d['county_areawater'] = s_geo[u'AREAWATER']
+                    d['county_arealand'] =  s_geo[u'AREALAND']
 
+                    col_list_=['block_group',
+                                'block_id',
+                                'tract_id',
+                                'county_id',
+                                'county_name',
+                                'lat',
+                                'lon',
+                                'state_code',
+                                'state_id',
+                                'state_name',
+                                'state_centlat',
+                                'state_centlon',
+                                'state_division',
+                                'state_region',
+                                'state_areawater',
+                                'state_arealand',
+                                'county_basename',
+                                'county_centlat',
+                                'county_centlon',
+                                'county_areawater',
+                                'county_arealand'
+                                ]
 
-                    col_list_=['block_group','block_id','tract_id','county_id'
-                               ,'county_name','lat','lon','state_code','state_id','state_name']
-
-                        
                     if use_column_id is True:
                         if id_as_int:
                             d[id_column]=int(id_)
                         else:
                             d[id_column]=id_
 
-
                     writer.write_row_dict(d)
-
-
 
                 except:
                     print 'Unable to find these coordinates in the US Census API: lat:%s , lon:%s' % (lat,lon)
